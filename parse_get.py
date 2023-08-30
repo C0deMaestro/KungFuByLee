fa768d1738fda06b89b25562246c74fbd93312b6
Arthur Balukhtin <artefact032@yandex.ru>
2023-08-31 00:05:06 +0300

    v1

--- parse_get.py
+++ parse_get.py
@@ -1,25 +1,29 @@
 def decode(http_request):
     rows = http_request.split("\n")
-    method,raw_path,req_params= rows[0].split()
-    type,http_version = req_params.split("/")
+    method, raw_path, req_params = rows[0].split()
+    type, http_version = req_params.split("/")
 
-    headers = {}
-
-    for indx,header in enumerate(rows[1:]):
-        if header.strip():
-            k,v = header.split(":")
-            headers[k] = v.strip()
-        else:
+    headers = []
+    last_indx = 0
+    for indx, header in enumerate(rows[1:]):
+        if not header.strip():
+            last_indx = indx
             break
+        k, v = header.split(":")
+        headers.append([k,v.strip()])
+
     request = {
         "type": type,
         "http_version": http_version,
         "method": method,
+        "path": raw_path.split("?")[0],
         "raw_path": raw_path,
-        "headers": [headers],}
-    if len(rows)>indx+2:
+        "headers": [headers],
+    }
+
+    if len(rows) > last_indx + 2:
         body = []
-        for body_str in rows[indx+2:]:
+        for body_str in rows[last_indx + 2 :]:
             if body_str.strip():
                 body.append(body_str)
 
@@ -27,7 +31,3 @@ def decode(http_request):
         request["body"] = body
 
     return request
-
-
-
-
