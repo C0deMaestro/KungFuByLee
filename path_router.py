365ec794215272c058877aaf6b3c82ae60ae938b
Arthur Balukhtin <artefact032@yandex.ru>
2023-08-31 16:58:59 +0300

    v1

---
+++ path_router.py
@@ -0,0 +1,21 @@
+from parse_get import decode
+from view import *
+import re
+def router(request):
+    path = request.get("path")
+    urls = [(r"/api/v1/books/",books_home),
+            (r"/api/v1/books/\d+/",books_item),
+            (r"/api/v1/books/\d+/authors/",books_authors),
+             (r"/api/v1/categories/",categoties),
+              (r"/api/v1/books/\d+/categories/",books_categories),]
+    for url in urls:
+        if re.fullmatch(url[0],path):
+            url[1](request)
+
+
+
+
+
+
+
+
