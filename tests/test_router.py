be4df57ec14930c6852a090a5699c4601bf0fb24
Arthur Balukhtin <artefact032@yandex.ru>
2023-09-01 17:22:47 +0300

    v1

--- tests/test_router.py
+++ tests/test_router.py
@@ -7,15 +7,9 @@ import pytest
     [
         (app.router_url({"path": "/api/v1/books/"}).__name__, books_home.__name__),
         (app.router_url({"path": "/api/v1/books/1337/"}).__name__, books_item.__name__),
-        (
-            app.router_url({"path": "/api/v1/books/1337/authors/"}).__name__,
-            books_authors.__name__,
-        ),
+        (app.router_url({"path": "/api/v1/books/1337/authors/"}).__name__,books_authors.__name__),
         (app.router_url({"path": "/api/v1/categories/"}).__name__, categories.__name__),
-        (
-            app.router_url({"path": "/api/v1/books/1337/categories/"}).__name__,
-            books_categories.__name__,
-        ),
+        (app.router_url({"path": "/api/v1/books/1337/categories/"}).__name__,books_categories.__name__),
     ],
 )
 def test1(test_input, expected):
